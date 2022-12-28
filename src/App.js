a64e21f6beeb379975f059e730e5e08ed4b71dea
baraql <baraqlip@gmail.com>
2022-12-28 14:33:33 -0800

    added firebase analytics

--- src/App.js
+++ src/App.js
@@ -14,10 +14,33 @@ import Typewriter from "typewriter-effect";
 import face from './assets/face.png';
 import { Link } from 'react-router-dom';
 import backgroundVideo from "./assets/background.gif";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4qxYIs0MyCsIkIhEDiXbDAU8LxK7-NKc",
+  authDomain: "website-4ba74.firebaseapp.com",
+  projectId: "website-4ba74",
+  storageBucket: "website-4ba74.appspot.com",
+  messagingSenderId: "539272362072",
+  appId: "1:539272362072:web:96a35bc3b3e5e4c2bbb22d",
+  measurementId: "G-HMVQ1MGWY3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // import backgroundImage from './assets/backgroundposter.png';
 // import YouTube from 'react-youtube';
 
-var app = document.getElementById('app');
+// var app = document.getElementById('app');
 
 // TODO add shower
 // TODO detect click anywhere on window and select textinput
